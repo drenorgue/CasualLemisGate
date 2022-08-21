67337cc94c6805194d4cdec6f6ca53470b794d3a
drenorgue <71455343+drenorgue@users.noreply.github.com>
2022-08-21 23:30:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Casual
+
+Developed with Unreal Engine 5
---
+++ Source/Casual/CasualGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CasualGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CASUAL_API ACasualGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
